88daa4dc0cece5a7ae1269330917db1cab83dc15
Kanat Tangwongsan <ktangwon@whiskas.westell.com>
2011-08-24 16:12:57 -0400

    rename it to mine

--- emacs.d/ktangwon/fonts.el
+++ emacs.d/ktangwon/fonts.el
@@ -1,12 +1,16 @@
 ;;; Fonts
 
 ;; From: http://community.schemewiki.org/cgi-bin/scheme.cgi?Emacs
+;;(and window-system
+;;     (create-fontset-from-fontset-spec
+;;      (concat
+;;       "-apple-monaco-medium-r-normal--18-*-*-*-*-*-fontset-monaco,"
+;;      "ascii:-apple-monaco-medium-r-normal--18-100-*-*-m-100-mac-roman"))
+;;     )
+;;
 (and window-system
-     (create-fontset-from-fontset-spec
-      (concat
-       "-apple-monaco-medium-r-normal--12-*-*-*-*-*-fontset-monaco,"
-       "ascii:-apple-monaco-medium-r-normal--12-100-*-*-m-100-mac-roman"))
-     )
+	(set-default-font "Apple Monaco-18")
+	)
 
 ;; To change a font size globally:
 ;; (set-face-attribute 'default nil :height 360)
